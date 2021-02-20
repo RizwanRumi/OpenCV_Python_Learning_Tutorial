19d90950442ed2a841a360cffdd28652736da8f1
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-20 18:22:11 +0100

    add convert color to scale video

--- getting_started_with_videos.py
+++ getting_started_with_videos.py
@@ -5,12 +5,18 @@ import cv2
 # multiple camera: set value 1 for 2nd camera, 2 for 3rd camera
 
 
-cap = cv2.VideoCapture(0);
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
 
-    cv2.imshow('frame', frame)
+
+    #convert color to gray scale video
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', gray)
+    # cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
