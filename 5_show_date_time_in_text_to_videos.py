e0d104a04cf13c5677b1f9b2a010b7e05e7f8bdf
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-20 22:58:28 +0100

    add show date time in text file

---
+++ 5_show_date_time_in_text_to_videos.py
@@ -0,0 +1,33 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+print(cap.get(3))  # cv2.CAP_PROP_FRAME_WIDTH = 3
+print(cap.get(4))  # cv2.CAP_PROP_FRAME_HEIGHT = 4
+
+# customize pixel setting and it will differ
+# cap.set(3, 5000)
+# cap.set(4, 300)
+
+# print(cap.get(3))
+# print(cap.get(4))
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if ret:
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('frame', gray)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text = 'Width: ' + str(cap.get(3)) + ' Height: ' + str(cap.get(4))
+        frame = cv2.putText(frame, text, (10, 50), font, 1,
+                            (0, 255, 255), 2, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
