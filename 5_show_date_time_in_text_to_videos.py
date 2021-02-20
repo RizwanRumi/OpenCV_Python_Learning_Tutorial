04df4be0afaeb1edc085ffaf67211b0dfa96afba
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-20 23:07:37 +0100

    add current date and time in videos

--- 5_show_date_time_in_text_to_videos.py
+++ 5_show_date_time_in_text_to_videos.py
@@ -1,4 +1,5 @@
 import cv2
+import datetime
 
 cap = cv2.VideoCapture(0)
 
@@ -20,7 +21,9 @@ while cap.isOpened():
 
         font = cv2.FONT_HERSHEY_SIMPLEX
         text = 'Width: ' + str(cap.get(3)) + ' Height: ' + str(cap.get(4))
-        frame = cv2.putText(frame, text, (10, 50), font, 1,
+        datet = str(datetime.datetime.now())
+
+        frame = cv2.putText(frame, datet, (10, 50), font, 1,
                             (0, 255, 255), 2, cv2.LINE_AA)
         cv2.imshow('frame', frame)
 
