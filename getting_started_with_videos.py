78a4a2b737933fb119d37c21d65964ea96aef92f
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-20 19:07:50 +0100

    save captured video

--- getting_started_with_videos.py
+++ getting_started_with_videos.py
@@ -6,25 +6,34 @@ import cv2
 
 
 cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
 # isOpened() return true or false about the directory of video file path
 print(cap.isOpened())
 
 while cap.isOpened():
     # while True:
+    # ret is a boolean variable if the frame is available
     ret, frame = cap.read()
 
-    # print some video property : cap.get()
-    print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    if ret == True:
+        # print some video property : cap.get()
+        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    #  convert color to gray scale video
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # save video
+        out.write(frame)
 
-    cv2.imshow('frame', gray)
-    # cv2.imshow('frame', frame)
+        #  convert color to gray scale video
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow('frame', gray)
+        # for color video -> cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
 cap.release()
