14c5d0f534f93ab9dececfe9e0ea5dc75db869d8
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-24 14:48:05 +0100

    add switch in trackbar

--- 12_bind_tracker_opencv.py
+++ 12_bind_tracker_opencv.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2 as cv
 
+
 def nothing(x):
     print(x)
+
 
 img = np.zeros((300, 512, 3), np.uint8)
 cv.namedWindow('image')
@@ -11,8 +13,10 @@ cv.createTrackbar('B', 'image', 0, 255, nothing)
 cv.createTrackbar('G', 'image', 0, 255, nothing)
 cv.createTrackbar('R', 'image', 0, 255, nothing)
 
+switch = '0 : OFF \n 1: ON'
+cv.createTrackbar(switch, 'image', 0, 1, nothing)
 
-while(1):
+while 1:
     cv.imshow('image', img)
     k = cv.waitKey(1) & 0xFF
     if k == 27:
@@ -21,7 +25,11 @@ while(1):
     b = cv.getTrackbarPos('B', 'image')
     g = cv.getTrackbarPos('G', 'image')
     r = cv.getTrackbarPos('R', 'image')
+    s = cv.getTrackbarPos(switch, 'image')
 
-    img[:] = [b, g, r]
+    if s == 0:
+        img[:] = 0
+    else:
+        img[:] = [b, g, r]
 
 cv.destroyAllWindows()
