ec1e043891338c6469747ae304e7a4a65762a0f5
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-24 19:03:36 +0100

    add threshold binary and binary inverse example

--- 13_threshold_image.py
+++ 13_threshold_image.py
@@ -1,3 +1,13 @@
 import cv2 as cv
 import numpy as np
 
+img = cv.imread('images/gradient.png')
+_, th1 = cv.threshold(img, 100, 255, cv.THRESH_BINARY)
+_, th2 = cv.threshold(img, 100, 255, cv.THRESH_BINARY_INV)
+
+cv.imshow('image', img)
+cv.imshow('th1', th1)
+cv.imshow('th2', th2)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
