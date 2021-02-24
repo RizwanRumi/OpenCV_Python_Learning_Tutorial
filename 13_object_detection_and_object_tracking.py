f93782d7031e662057e3d8e1b70cf7453a6418b0
RizwanRumi <rizwan.cse33@gmail.com>
2021-02-24 18:10:40 +0100

    add lower and upper HSV value in masking function by using Trackbar

--- 13_object_detection_and_object_tracking.py
+++ 13_object_detection_and_object_tracking.py
@@ -4,26 +4,45 @@ import numpy as np
 def nothing(x):
     pass
 
-# cv2.namedWindow("Tracking")
+cv2.namedWindow("Tracking")
+
+cv2.createTrackbar("LH", "Tracking", 0, 360, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 360, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 360, nothing)
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 2555, 255, nothing)
 
 while True:
     frame = cv2.imread('images/smarties.png')
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    l_b = np.array([110, 50, 50]) # lower blue hsv
-    u_b = np.array([130, 255, 255]) # upper limit blue hsv
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    # l_b = np.array([110, 50, 50]) # lower blue hsv
+    # u_b = np.array([130, 255, 255]) # upper limit blue hsv
+
+    # GET LOWER HSV and UPPER HSV from Trackbar
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
 
     mask = cv2.inRange(hsv, l_b, u_b)
 
     res = cv2.bitwise_and(frame, frame, mask=mask)
 
     cv2.imshow("frame", frame)
-    cv2.imshow("hsv", hsv)
+    # cv2.imshow("hsv", hsv)
     cv2.imshow("mask", mask)
     cv2.imshow("res", res)
 
-    key = cv2.waitKey(1) 
+    key = cv2.waitKey(1)
     if key == 27:
         break
 
