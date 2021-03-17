8507c6d67340c052526a3ad99c3e1e2b5141b388
RizwanRumi <rizwan.cse33@gmail.com>
2021-03-17 19:21:23 +0100

    add trackbar example

--- 20_Canny_Edge_detection.py
+++ 20_Canny_Edge_detection.py
@@ -1,10 +1,11 @@
-import cv2
+import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('images/messi5.jpg', 0)
 
-canny = cv2.Canny(img, 100, 200)
+img = cv.imread('images/messi5.jpg', 0)
+
+canny = cv.Canny(img, 100, 200)
 
 titles = ['image', 'canny']
 images = [img, canny]
@@ -15,3 +16,31 @@ for i in range(2):
     plt.xticks([]), plt.yticks([])
 
 plt.show()
+
+'''
+def nothing(x):
+    print(x)
+
+cv.namedWindow('image')
+
+cv.createTrackbar('start', 'image', 0, 200, nothing)
+cv.createTrackbar('end', 'image', 0, 300, nothing)
+
+while 1:
+    img = cv.imread('images/messi5.jpg', 0)
+    start = cv.getTrackbarPos('start', 'image')
+    end = cv.getTrackbarPos('end', 'image')
+
+    k = cv.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    if start == 0 and end == 0:
+        pass
+    else:
+        img = cv.Canny(img, start, end)
+
+    img = cv.imshow('image', img)
+    
+cv.destroyAllWindows()
+'''
