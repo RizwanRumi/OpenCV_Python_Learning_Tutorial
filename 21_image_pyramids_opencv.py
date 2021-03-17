863bd00b71f2120bc47e96549ad6b505d1a1c051
RizwanRumi <rizwan.cse33@gmail.com>
2021-03-18 00:53:36 +0100

    re-write gausian pyramid example by for loop

--- 21_image_pyramids_opencv.py
+++ 21_image_pyramids_opencv.py
@@ -9,11 +9,27 @@ is subject to repeated smoothing and subsampling.
 '''
 # Gaussian pyramid repeats filtering and subsampling the image
 img = cv2.imread("images/lena.jpg")
+'''
 lr1 = cv2.pyrDown(img)
 lr2 = cv2.pyrDown(lr1)
+hr2 = cv2.pyrUp(lr2) # make low level image or blur image
 
 cv2.imshow("Original image", img)
 cv2.imshow("PyrDown 1 image", lr1)
 cv2.imshow("PyrDown 2 image", lr2)
+cv2.imshow("PyrUp 1 image", hr2)
+'''
+# re-write this code using for loop
+
+cv2.imshow("Original image", img)
+
+layer = img.copy()
+gp = [layer]
+
+for i in range(6):
+    layer = cv2.pyrDown(layer)
+    gp.append(layer)
+    cv2.imshow(str(i), layer)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
