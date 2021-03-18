91264dfa30a55afecdabbe30a48a8861b8d25bfb
RizwanRumi <rizwan.cse33@gmail.com>
2021-03-18 01:04:36 +0100

    add laplacian pyramid example

--- 21_image_pyramids_opencv.py
+++ 21_image_pyramids_opencv.py
@@ -6,6 +6,8 @@ Pyramid: multi-scale signal representation in which a signal
 is subject to repeated smoothing and subsampling.
 1. Laplacian pyramid
 2. Gaussian pyramid
+
+It helps us to blend the images and the reconstruction of the images
 '''
 # Gaussian pyramid repeats filtering and subsampling the image
 img = cv2.imread("images/lena.jpg")
@@ -29,7 +31,22 @@ gp = [layer]
 for i in range(6):
     layer = cv2.pyrDown(layer)
     gp.append(layer)
-    cv2.imshow(str(i), layer)
+    #cv2.imshow(str(i), layer)
+
+# Laplacian: A level in Laplacian Pyramid is formed by the difference
+# between that level in gaussian pyramid and expanded version of its
+# upper level in Gaussian pyramid.
+
+layer = gp[5]
+cv2.imshow('upper level Gaussian Pyramid', layer)
+lp = [layer]
+
+for i in range(5, 0, -1):
+    gaussian_extended = cv2.pyrUp(gp[i])
+    laplacian = cv2.subtract(gp[i-1],gaussian_extended)
+    cv2.imshow(str(i), laplacian)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
