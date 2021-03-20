5e61c9174d8c1ed5a0df7ba52d6b149bcc069852
RizwanRumi <rizwan.cse33@gmail.com>
2021-03-20 21:07:56 +0100

    add histogram example for color himage

--- 26_histogram_opencv.py
+++ 26_histogram_opencv.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread("images/lena.jpg")
+img = cv.imread("images/lena.jpg", -1)
 #img = np.zeros((200,200), np.uint8)
 #cv.rectangle(img, (0, 100), (200, 200), (255), -1)
 #cv.rectangle(img, (0, 50), (100, 100), (127), -1)
@@ -18,8 +18,17 @@ img = cv.imread("images/lena.jpg")
 #plt.hist(g.ravel(), 256, [0, 256])
 #plt.hist(r.ravel(), 256, [0, 256])
 
-hist = cv.calcHist([img], [0], None, [256], [0, 256])
-plt.plot(hist)
+# hist = cv.calcHist([img], [0], None, [256], [0, 256])
+# plt.plot(hist)
+# plt.show()
+
+color = ('b', 'g', 'r')
+
+for channel,col in enumerate(color):
+    hist = cv.calcHist([img], [channel], None, [256], [0, 256])
+    plt.plot(hist, color = col)
+    plt.xlim([0,256])
+plt.title("Histogram for color scale picture")
 plt.show()
 
 cv.waitKey(0)
