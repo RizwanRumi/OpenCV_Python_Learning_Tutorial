a32e214f7d48a8c4ca5899d1bdce20157cee8a89
RizwanRumi <rizwan.cse33@gmail.com>
2021-03-22 17:36:19 +0100

    add desciption of loc

--- 27_template_matching.py
+++ 27_template_matching.py
@@ -15,10 +15,21 @@ res = cv2.matchTemplate(gray_image, template_image, cv2.TM_CCOEFF_NORMED)
 print(res)
 
 threshold = 0.9
-loc = np.where(res >=threshold)
+loc = np.where(res >= threshold)
+'''
+The variable loc contains a tuple of an array of indexes: ((array([row], dtype), array([column], dtype)).  
+This is equivalent to ((y_pos, x_pos))
+Using zip this tuple is converted to a list of coordinates of the form [(row, column)] as in [(y_pos, x_pos)]
+The -1 at the end reverses this list to [(column, row)] to obtain (x_pos, y_pos).
+'''
 print(loc)
+print('difference:')
+print(*loc)
+
+
 
 for pt in zip(*loc[::-1]):
+    print(pt)
     cv2.rectangle(img, pt, (pt[0]+w, pt[1]+h), (0,255,0), 2)
 
 
